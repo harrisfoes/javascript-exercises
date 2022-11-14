834ecb11bad7c52e8763376b9d312a23673e136a
harrisfoes <11287035+harrisfoes@users.noreply.github.com>
2022-11-14 08:51:58 +0800

    exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,22 @@
-const removeFromArray = function() {
+const removeFromArray = function(myArray, ...toRemove) {
+    const args = Array.from(toRemove);
+    console.log(args);
+    console.log(myArray);
+
+    let returnArray = myArray;
+
+    for(i of args){
+        console.log(`i: ${i}`)
+        for(let j = 0;j<myArray.length;j++){
+            console.log(`char: ${myArray[j]}`);
+            if(i === myArray[j]){
+                returnArray.splice(j,1);
+                console.log(returnArray);
+            }
+        }
+    }
+
+    return returnArray;
 
 };
 

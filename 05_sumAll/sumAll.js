79a2d4f2d5c151580575745a84b5db30fc645ba7
harrisfoes <11287035+harrisfoes@users.noreply.github.com>
2022-12-30 20:30:43 +0800

    exercise5 solution

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,20 @@
-const sumAll = function() {
+const sumAll = function(smol, large) {
+ let sum = 0;
+ 
+ if( !Number.isInteger(smol) || smol < 0 || !Number.isInteger(large) || large < 0)
+    return 'ERROR';
+ 
+ if (smol > large){
+    let temp = smol;
+    smol = large;
+    large = temp;
+ }
+
+ for(let i = smol; i<=large; i++){
+    sum += i;
+ }
+
+ return sum;
 
 };
 

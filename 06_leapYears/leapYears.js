98d8fb43695f9470321040c3fbe682990fcd9598
harrisfoes <11287035+harrisfoes@users.noreply.github.com>
2022-12-31 19:56:30 +0800

    add leapyears 06 solution

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,18 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    if(year % 4 != 0){ //divisible by 4 only
+        return false;
+    }
+    
+    if(year % 100 == 0){
+        if (year % 400 == 0)
+            return true;
+        else 
+            return false;
+    }
+    return true;
 
 };
+
 
 // Do not edit below this line
 module.exports = leapYears;

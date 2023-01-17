c566929bece1b802ece5cf6d149cb8da0c4dae2e
harrisfoes <11287035+harrisfoes@users.noreply.github.com>
2023-01-17 19:32:49 +0800

    code cleanup

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,17 +1,12 @@
 const removeFromArray = function(myArray, ...toRemove) {
     const args = Array.from(toRemove);
-    console.log(args);
-    console.log(myArray);
 
     let returnArray = myArray;
 
     for(i of args){
-        console.log(`i: ${i}`)
         for(let j = 0;j<myArray.length;j++){
-            console.log(`char: ${myArray[j]}`);
             if(i === myArray[j]){
                 returnArray.splice(j,1);
-                console.log(returnArray);
             }
         }
     }
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,11 @@
 const ftoc = function(num) {
   const ans = (num - 32) * (5 / 9);
-  return Math.round(ans * 10) / 10
+  return Math.round(ans * 10) / 10;
 };
 
 const ctof = function(num) {
   const ans = (num * (9 / 5)) + 32;
-  return Math.round(ans * 10) / 10
+  return Math.round(ans * 10) / 10;
 };
 
 // Do not edit below this line

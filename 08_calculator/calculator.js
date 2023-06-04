4e7d0255eee59d7c4f96457ae444b6811afb233b
harrisfoes <11287035+harrisfoes@users.noreply.github.com>
2023-06-04 12:22:02 +0800

    calculator solution

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,43 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a - b
 };
 
-const sum = function() {
-	
+const sum = function(arrayNum) {
+  let result = 0;
+	for(i in arrayNum){
+    result += arrayNum[i];
+  }
+
+  return result
 };
 
-const multiply = function() {
+const multiply = function(arrayNum) {
+  let result = 1;
+	for(i in arrayNum){
+    result *= arrayNum[i];
+  }
 
+  return result
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return Math.pow(a,b)
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let ans = num
+
+  if(num == 0)
+    return 1;
+
+  for(i = num - 1; i > 0 ; i--){
+   ans *= i; 
+  }	
+  return ans;
 };
 
 // Do not edit below this line

754167af0c67e63a4f0909449462981fb28a3ebe
harrisfoes <11287035+harrisfoes@users.noreply.github.com>
2023-06-04 17:29:01 +0800

    finished last two exercises

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const ages = people.sort(function(a,b) {
+        let a_yearOfDeath = a.yearOfDeath;
+        let b_yearOfDeath = b.yearOfDeath;
 
+        if(a_yearOfDeath == null)
+            a_yearOfDeath = new Date().getFullYear();
+        if(b_yearOfDeath == null)
+            b_yearOfDeath = new Date().getFullYear();
+         
+        const age_a = a_yearOfDeath - a.yearOfBirth;
+        const age_b = b_yearOfDeath - b.yearOfBirth;
+
+        return age_b - age_a;
+    });
+
+    return ages[0];
 };
 
 // Do not edit below this line

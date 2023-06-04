56a224253c03318aa2cb13a2344dce1871a157f6
harrisfoes <11287035+harrisfoes@users.noreply.github.com>
2023-06-04 16:34:37 +0800

    fibonacci result

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    let x = 0, y = 1, lastnum = 1;
+    let num = parseInt(number);
 
+    if(num < 0)
+        return "OOPS"
+
+    for(i = num; i>1; i--){
+        lastnum = x + y;
+        //console.log(i);
+        //console.log(lastnum);
+        x = y;
+        y = lastnum;
+    }
+    return lastnum
 };
 
 // Do not edit below this line

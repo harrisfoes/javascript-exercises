d3b9960a1fde5fa4fd150b582b1afe7e1c55ca27
harrisfoes <11287035+harrisfoes@users.noreply.github.com>
2023-06-04 16:05:51 +0800

    added solution for palindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,17 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    const filteredWord = word.split(' ').join('').replace(/[^a-z0-9_]/gi,'').toLowerCase();
+    const reverseWord = filteredWord.split("").reverse();
+    let palindromCheck = false;
+    let matchCount = 0;
+
+    for(let i in filteredWord){
+        if(filteredWord[i] == reverseWord[i])
+            matchCount +=1;
+    }
+    if(matchCount == filteredWord.length)
+        palindromCheck = true;
+    
+    return palindromCheck;
 
 };
 
